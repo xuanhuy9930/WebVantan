4186e41f2d36b91e3bd43339d9320ed8e5d515e5
xuanhuy9930 <xuanhuy9930@gmail.com>
2019-09-17 15:36:51 +0700

    up

---
+++ resources/views/header.blade.php
@@ -0,0 +1,241 @@
+
+<div id="page">
+		<header id="header">
+			<div class="task-1">
+				<div class="inner-task container">
+					<span><i class="fas fa-map-marker-alt"></i></span>
+					<p >Số 1 - B8 Khu đô thị Mỹ Đình 1 - Q. Nam Từ Liêm - Hà Nội (Đối diện cổng trường THCS Đoàn Thị Điểm )</p>
+
+					<span><i class="fas fa-phone"></i></span>
+					<p>(024) 22 655 666</p>
+
+					<span><i class="far fa-envelope"></i></span>
+					<p>baogia@inhongdang.vn</p>
+					<span><i class="fab fa-facebook-f"></i></span>
+					<span><i class="fab fa-google-plus-g"></i></span>
+					<span><i class="fab fa-twitter"></i></span>
+				</div> 
+			</div>
+
+			<nav class="navigations logo">
+		      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		          <a href="" class="navbar-brand">
+		            <img src="images/logo1.png" alt="" width="210px" height="87px">
+		          </a>
+		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		        <span class="navbar-toggler-icon"></span>
+		      </button>
+		      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+		          <ul class="navbar-nav">
+		            <li class="nav-item active">
+		              <a class="nav-link" href="#"><i class="fas fa-home"></i>Trang Chủ<span class="sr-only">(current)</span></a>
+		            </li>                     
+		            <li class="nav-item dropdown">
+		              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		              	<i class="fas fa-globe-europe"></i>
+		                Giới Thiệu
+		              </a>
+		              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+		                <ul class="drop-item">
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit </span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit </span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                </ul>
+		                
+		              </div>
+		            </li>
+
+		            <li class="nav-item dropdown">
+		              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		              	<i class="fas fa-cog"></i>
+		                Dịch Vụ
+		              </a>
+		              <!-- Dich Vu -->
+		              <div class="dropdown-menu new-css" aria-labelledby="navbarDropdownMenuLink">
+		                  <div class="row">
+		                      <div class="col-md-3 menu-down">
+		                        <a class="dropdown-item" href="#">Ấn Phẩm Văn Phòng</a>
+		                        <ul class="multi-column-dropdown">
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                                        visit</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Phong
+		                                        Bì</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Kẹp
+		                                        File</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In hóa
+		                                        đơn</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sách,
+		                                        In tạp chí</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sổ
+		                                        tay, sổ bìa da</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In thiệp chúc mừng năm
+		                                        mới</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In tiêu đề
+		                                        thư</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In nhãn
+		                                        đĩa</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In giấy ghi
+		                                        chú</span></a></li>
+		                        </ul>
+		                      </div>
+		                      <div class="col-md-3 menu-down">
+		                        <a class="dropdown-item" href="#">Ấn phẩm quảng cáo</a>
+		                        <ul class="multi-column-dropdown">
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                                        visit</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Phong
+		                                        Bì</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Kẹp
+		                                        File</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In hóa
+		                                        đơn</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sách,
+		                                        In tạp chí</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sổ
+		                                        tay, sổ bìa da</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In thiệp chúc mừng năm
+		                                        mới</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In tiêu đề
+		                                        thư</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In nhãn
+		                                        đĩa</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In giấy ghi
+		                                        chú</span></a></li>
+		                        </ul>
+		                      </div>
+		                      <div class="col-md-3 menu-down">
+		                        <a class="dropdown-item" href="#">Ấn Phẩm Khác</a>
+		                        <ul class="multi-column-dropdown">
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                                        visit</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Phong
+		                                        Bì</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Kẹp
+		                                        File</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In hóa
+		                                        đơn</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sách,
+		                                        In tạp chí</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sổ
+		                                        tay, sổ bìa da</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In thiệp chúc mừng năm
+		                                        mới</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In tiêu đề
+		                                        thư</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In nhãn
+		                                        đĩa</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In giấy ghi
+		                                        chú</span></a></li>
+		                        </ul>
+		                      </div>
+		                      <div class="col-md-3 menu-down">
+		                        <a class="dropdown-item" href="#">Thiết Kế Quảng Cáo</a>
+		                        <ul class="multi-column-dropdown">
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                                        visit</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Phong
+		                                        Bì</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In Kẹp
+		                                        File</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In hóa
+		                                        đơn</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sách,
+		                                        In tạp chí</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In sổ
+		                                        tay, sổ bìa da</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In thiệp chúc mừng năm
+		                                        mới</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In tiêu đề
+		                                        thư</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In nhãn
+		                                        đĩa</span></a></li>
+		                            <li><a href="#"><i class="fas fa-star"></i><span>In giấy ghi
+		                                        chú</span></a></li>
+		                        </ul>
+		                      </div>
+		                    </div>
+		              </div>
+		            </li>
+
+		            <li class="nav-item dropdown">
+		              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		              	<i class="fas fa-globe-europe"></i>
+		                Báo Giá
+		              </a>
+		              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+		                <ul class="drop-item">
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit </span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit </span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                </ul>
+		              </div>
+		            </li>
+
+		            <li class="nav-item">				            	
+		              <a class="nav-link" href="#"><i class="fas fa-question"></i> Hướng Dẫn</a>
+		            </li>
+
+		            <li class="nav-item">
+		            	
+		              <a class="nav-link" href="#"><i class="fas fa-question"></i> Sản Phẩm</a>
+		            </li>
+
+		            <li class="nav-item dropdown">
+		              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		              	<i class="far fa-newspaper"></i>
+		                Tin Tức
+		              </a>
+		              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+		                <ul class="drop-item">
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit </span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit </span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                	<li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                    <li><a href="#"><i class="fas fa-star"></i><span>In card
+		                    visit</span></a></li>
+		                </ul>
+		              </div>
+		            </li>
+
+		            <li class="nav-item">
+		              <a class="nav-link" href="#">
+		              <i class="fas fa-map-marker-alt"></i> Liên Hệ</a>
+		            </li>
+
+		          </ul>
+		        </div>
+		      </nav>
+			</nav>
+		</header>
+	</div>
+		
---
+++ resources/views/master-layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+    <!-- font-chu -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap" rel="stylesheet">
+    <!-- slicebox -->
+    <link rel="stylesheet" type="text/css" href="css/slicebox.css" />
+    <script type="text/javascript" src="js/modernizr.custom.46884.js"></script>
+    <!-- css fancy -->
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+
+    <!-- carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.css">
+    @yield('css')
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+    @include('header')
+    @yield('content')
+    @include('footer')
+</body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="js/jquery.slicebox.js"></script>
+    <script src="js/jquery.fancybox.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/trangchu.js"></script>
+    @yield('js')
+</html>
